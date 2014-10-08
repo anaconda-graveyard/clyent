a9814ac23412fd8aaa4054e3b5b83764e0708028
srossross <srossross@gmail.com>
2014-10-08 14:01:36 -0700

    ENH: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clyent',
-    version="0.2.3",
+    version="0.3.0",
     author='Continuum Analytics',
     author_email='srossross@gmail.com',
     url='http://github.com/binstar/clyent',
