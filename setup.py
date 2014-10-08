ce8e63da2a5738c3b9eeb8282413d5f37c61409d
srossross <srossross@gmail.com>
2014-10-08 09:08:19 -0700

    bumped version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clyent',
-    version="0.2.2",
+    version="0.2.3",
     author='Continuum Analytics',
     author_email='srossross@gmail.com',
     url='http://github.com/binstar/clyent',
