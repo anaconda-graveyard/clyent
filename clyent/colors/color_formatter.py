7cb2e9dc49939b20cc074f42a7ecd53993e9fe6b
Derek Ludwig <dludwig@continuum.io>
2016-04-05 10:37:49 -0700

    Add new signature for _vformat

--- clyent/colors/color_formatter.py
+++ clyent/colors/color_formatter.py
@@ -34,39 +34,102 @@ class ColorFormatStream(string.Formatter):
         else:
             return string.Formatter.get_field(self, field_name, args, kwargs)
 
+    # The signature of _vformat changed in Python 3.4. It added a new argument
+    # `auto_arg_index`.
+    # https://github.com/python/cpython/blob/3.4/Lib/string.py#L192
+    # https://github.com/python/cpython/blob/2.7/Lib/string.py#L567
+    if sys.version_info[:2] > (3, 3):
+        def _vformat(self, format_string, args, kwargs, used_args, recursion_depth,
+                     auto_arg_index=0):
+            if recursion_depth < 0:
+                raise ValueError('Max string recursion exceeded')
+            result = []
+            for literal_text, field_name, format_spec, conversion in \
+                    self.parse(format_string):
 
-    def _vformat(self, format_string, args, kwargs, used_args, recursion_depth):
-        if recursion_depth < 0:
-            raise ValueError('Max string recursion exceeded')
-        result = []
+                # output the literal text
+                if literal_text:
+                    # CHANGED: write instead of append
+                    self.stream.write(literal_text)
 
-        for literal_text, field_name, format_spec, conversion in \
-                self.parse(format_string):
+                # if there's a field, output it
+                if field_name is not None:
+                    # this is some markup, find the object and do
+                    #  the formatting
 
-            # output the literal text
-            self.stream.write(literal_text)
+                    # handle arg indexing when empty field_names are given.
+                    if field_name == '':
+                        if auto_arg_index is False:
+                            raise ValueError('cannot switch from manual field '
+                                             'specification to automatic field '
+                                             'numbering')
+                        field_name = str(auto_arg_index)
+                        auto_arg_index += 1
+                    elif field_name.isdigit():
+                        if auto_arg_index:
+                            raise ValueError('cannot switch from manual field '
+                                             'specification to automatic field '
+                                             'numbering')
+                        # disable auto arg incrementing, if it gets
+                        # used later on, then an exception will be raised
+                        auto_arg_index = False
 
-            # if there's a field, output it
-            if field_name is not None:
-                # this is some markup, find the object and do
-                #  the formatting
+                    # given the field_name, find the object it references
+                    #  and the argument it came from
+                    obj, arg_used = self.get_field(field_name, args, kwargs)
+                    used_args.add(arg_used)
 
-                # given the field_name, find the object it references
-                #  and the argument it came from
-                obj, arg_used = self.get_field(field_name, args, kwargs)
-                used_args.add(arg_used)
+                    # do any conversion on the resulting object
+                    obj = self.convert_field(obj, conversion)
 
-                # do any conversion on the resulting object
-                obj = self.convert_field(obj, conversion)
+                    # expand the format spec, if needed
+                    format_spec, auto_arg_index = string.Formatter._vformat(
+                        self, format_spec, args, kwargs,
+                        used_args, recursion_depth-1,
+                        auto_arg_index=auto_arg_index)
 
-                # expand the format spec, if needed
-                format_spec = string.Formatter()._vformat(format_spec, args, kwargs,
-                                                          used_args, recursion_depth - 1)
+                    # format the object and append to the result
+                    # CHANGED: remove append
+                    self.format_field(obj, format_spec)
 
-                # format the object and append to the result
-                self.format_field(obj, format_spec)
+            return ''.join(result), auto_arg_index
+    else:
+        def _vformat(self, format_string, args, kwargs, used_args, recursion_depth,
+                     **extras):
+            if recursion_depth < 0:
+                raise ValueError('Max string recursion exceeded')
+            result = []
 
-        return ''.join(result)
+            for literal_text, field_name, format_spec, conversion in \
+                    self.parse(format_string):
+
+                # output the literal text
+                if literal_text:
+                    # CHANGED: write instead of append
+                    self.stream.write(literal_text)
+
+                # if there's a field, output it
+                if field_name is not None:
+                    # this is some markup, find the object and do
+                    #  the formatting
+
+                    # given the field_name, find the object it references
+                    #  and the argument it came from
+                    obj, arg_used = self.get_field(field_name, args, kwargs)
+                    used_args.add(arg_used)
+
+                    # do any conversion on the resulting object
+                    obj = self.convert_field(obj, conversion)
+
+                    # expand the format spec, if needed
+                    format_spec = string.Formatter._vformat(self, format_spec, args, kwargs,
+                                                            used_args, recursion_depth - 1)
+
+                    # format the object and append to the result
+                    # CHANGED: remove append
+                    self.format_field(obj, format_spec)
+
+            return ''.join(result)
 
 def print_colors(text='', *args, **kwargs):
     '''
